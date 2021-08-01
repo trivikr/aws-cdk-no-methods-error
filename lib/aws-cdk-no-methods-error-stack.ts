7fad4fef781eb8ac6c4490c5dd3176238a7db84c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-08-01 16:53:34 -0700

    chore: call lambda function as integration from API Gateway

--- lib/aws-cdk-no-methods-error-stack.ts
+++ lib/aws-cdk-no-methods-error-stack.ts
@@ -1,10 +1,16 @@
 import * as cdk from "@aws-cdk/core";
+import * as apigw from "@aws-cdk/aws-apigateway";
 import * as lambda from "@aws-cdk/aws-lambda-nodejs";
 
 export class AwsCdkNoMethodsErrorStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.NodejsFunction(this, "handler");
+    const api = new apigw.RestApi(this, "endpoint");
+    const test = api.root.addResource("test");
+    test.addMethod(
+      "GET",
+      new apigw.LambdaIntegration(new lambda.NodejsFunction(this, "handler"))
+    );
   }
 }
