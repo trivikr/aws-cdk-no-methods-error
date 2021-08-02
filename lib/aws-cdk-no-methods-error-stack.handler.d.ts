08ae526a339dc37f242a629a966f40f0cefb641c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-08-01 17:01:10 -0700

    chore: delete transiled files

--- lib/aws-cdk-no-methods-error-stack.handler.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const handler: () => Promise<{
-    statusCode: number;
-    headers: {
-        "Access-Control-Allow-Origin": string;
-        "Access-Control-Allow-Credentials": boolean;
-    };
-    body: string;
-}>;
